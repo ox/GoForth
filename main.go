6b8ecaf2694a17bfe2ca07a72a5a1b2bf5a41b9a
Artem Titoulenko <artem.titoulenko@gmail.com>
2011-06-10 17:33:47 -0400

    it's a working repl now.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"strings";
 	"bufio";
 	"flag";
+	"fmt";
 	"container/vector";
 )
 
@@ -21,11 +22,11 @@ var file *string = flag.String("file", "", "Define a Fourth src file to be read"
 func main() {
 	flag.Parse();
 	
-	DataStack := vector.New(0);
+	DataStack := new (vector.Vector);
 	
 	if *file != "" {
-		file_to_read, err := os.Open(*file, os.O_RDONLY, 0755); 
-		if file_to_read == nil { log.Stderr(err); }
+		file_to_read, err := os.OpenFile(*file, os.O_RDONLY, 0755); 
+		if file_to_read == nil { log.Fatal(err); }
 		
 		in = bufio.NewReader( file_to_read );
 		
@@ -33,7 +34,7 @@ func main() {
 		
 		for {
 			dat, err := in.ReadString(' ');
-			if err != nil { log.Stdout("ok"); return; }
+			if err != nil { log.Fatal("can't read file string"); return; }
 			
 			if dat[0:len(dat)-1] == "(" { 
 				comment = true;
@@ -52,18 +53,21 @@ func main() {
 
 		for {
 			fmt.Print("> ");
-			read, err : buf.ReadString('\n')
+			read, err := buf.ReadString('\n')
 			if err != nil {
 				println()
 				break
 			}
+			
+			comms := strings.Split(read," ", -1)
+			
+			for i := 0; i < len(comms); i++ {
+				if len(comms) == 0 || comms[i] == "" || comms[i] == " " {
+					continue
+				}
 
-			line := read[0: len(read)-1];
-			if len(line) == 0 {
-				continue
+				parse_forth(strings.TrimSpace(comms[i]), DataStack)
 			}
-
-			parse_forth(line, DataStack)
 		}
 	}
 }
@@ -84,7 +88,7 @@ func main() {
 
 func check_stack_size( DataStack *vector.Vector, required int) bool {
 	if DataStack.Len() < required {
-		log.Stderr("Stack depth is less then " + string(required) + ". Operation is impossible");
+		log.Fatal("Stack depth is less then " + string(required) + ". Operation is impossible");
 		return false;
 	} 
 	
@@ -93,7 +97,7 @@ func check_stack_size( DataStack *vector.Vector, required int) bool {
 
 func parse_forth(dat string, DataStack *vector.Vector) {
 	L := DataStack.Len();
-
+	
 	switch strings.TrimSpace(string(dat)) {
 		case "":
 		case "<cr>":
@@ -101,43 +105,43 @@ func parse_forth(dat string, DataStack *vector.Vector) {
 		case "t":
 			//check the DataStack size using the popped value
 			//	if it passes, then the program continues
-			minimum := int(DataStack.Pop().(float));
+			minimum := int(DataStack.Pop().(float32));
 			if DataStack.Len() < minimum {
-				log.Stderr("DataStack has not enough minerals (values)");
+				log.Println("DataStack has not enough minerals (values)");
 			}
 		case ".":
-			log.Stdout(DataStack.Pop());
+			log.Println(DataStack.Pop());
 		case "0SP":
 			DataStack.Cut(0, L);
 		case ".S":
-			log.Stdout(DataStack);
+			log.Println(DataStack);
 		case "2/":
-			DataStack.Push( DataStack.Pop().(float) / 2);
+			DataStack.Push( DataStack.Pop().(float32) / 2);
 		case "2*":
-			DataStack.Push( DataStack.Pop().(float) * 2);
+			DataStack.Push( DataStack.Pop().(float32) * 2);
 		case "2-":
-			DataStack.Push( DataStack.Pop().(float) - 2);
+			DataStack.Push( DataStack.Pop().(float32) - 2);
 		case "2+":
-			DataStack.Push( DataStack.Pop().(float) + 2);
+			DataStack.Push( DataStack.Pop().(float32) + 2);
 		case "1-":
-			DataStack.Push( DataStack.Pop().(float) - 1);
+			DataStack.Push( DataStack.Pop().(float32) - 1);
 		case "1+":
-			DataStack.Push( DataStack.Pop().(float) + 1);
+			DataStack.Push( DataStack.Pop().(float32) + 1);
 		case "DUP":
 			DataStack.Push( DataStack.Last() );
 		case "?DUP":
-			if DataStack.Last().(float) != 0 { DataStack.Push( DataStack.Last().(float) ); }
+			if DataStack.Last().(float32) != 0 { DataStack.Push( DataStack.Last().(float32) ); }
 		case "PICK":
-			number := int(DataStack.Pop().(float)) ;
+			number := int(DataStack.Pop().(float32)) ;
 			
 			if number < L {
-				DataStack.Push( DataStack.At(L - 1 - number).(float) );
+				DataStack.Push( DataStack.At(L - 1 - number).(float32) );
 			} else {
-				log.Stderr("picking out of stack not allowed. Stack Length: " + string(L) + ". Selecting: " + string(number) + ".");
+				log.Fatal("picking out of stack not allowed. Stack Length: " + string(L) + ". Selecting: " + string(number) + ".");
 				return;
 			}
 		case "TUCK":
-			DataStack.Insert(L - 2, int(DataStack.Last().(float)) );
+			DataStack.Insert(L - 2, int(DataStack.Last().(float32)) );
 		case "NIP":
 			DataStack.Delete(L - 2);
 		case "2DROP":
@@ -153,20 +157,20 @@ func parse_forth(dat string, DataStack *vector.Vector) {
 			l := DataStack.Len();
 			DataStack.Swap(l - 2, l - 1);
 		case "*":
-			num1 := DataStack.Pop().(float);
-			num2 := DataStack.Pop().(float);
+			num1 := DataStack.Pop().(float32);
+			num2 := DataStack.Pop().(float32);
 			DataStack.Push( num1 * num2 );				
 		case "+":
-			num1 := DataStack.Pop().(float);
-			num2 := DataStack.Pop().(float);
+			num1 := DataStack.Pop().(float32);
+			num2 := DataStack.Pop().(float32);
 			DataStack.Push( num1 + num2 );
 		case "-":
-			num1 := DataStack.Pop().(float);
-			num2 := DataStack.Pop().(float);
+			num1 := DataStack.Pop().(float32);
+			num2 := DataStack.Pop().(float32);
 			DataStack.Push( num2 - num1 );
 		case "/":
-			num1 := DataStack.Pop().(float);
-			num2 := DataStack.Pop().(float);
+			num1 := DataStack.Pop().(float32);
+			num2 := DataStack.Pop().(float32);
 			DataStack.Push( num2 / num1 );
 		case "-ROT":
 			DataStack.Swap(L - 1, L - 2);
@@ -181,15 +185,15 @@ func parse_forth(dat string, DataStack *vector.Vector) {
 			DataStack.Swap(L - 4, L - 2);
 			DataStack.Swap(L - 3, L - 1);
 		case "EMIT":
-			log.Stdout( string([]byte{uint8(DataStack.Last().(float))}) );
+			log.Println( string([]byte{uint8(DataStack.Last().(float32))}) );
 		default:
-			val, ok := strconv.Atof(dat);
+			val, ok := strconv.Atof32(dat);
 			
 			if ok == nil {
 				DataStack.Push( val );
 			} else {
-				log.Stderr(ok);
-				log.Stderr("error, unknown token \""+dat+"\"");
+				log.Println(ok);
+				log.Fatalln("error, unknown token \""+dat+"\"");
 			}
 	}
 } 
